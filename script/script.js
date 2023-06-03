28917bf6baea8adab9118c7d94e40e707949485c
bruno-bueno <bbueno1122@gmail.com>
2023-06-03 19:39:42 -0300

    commit-interface-1

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+var h3Opcao=document.getElementById("opcao");
+var opp1=document.getElementById("opp1");
+var opp2=document.getElementById("opp2");
+var jogoTela=document.getElementById("jogo");
+
 var heroi ={
     nome: "Wilson",
     vida: 100,
@@ -16,24 +21,31 @@ var armaAdversario={
     dano: 5,
 }
 var vezesAtacado=0;
+var quemJoga=gerarNumeroAleatorio(0, 2);
 
 function main(){
+    var h3Jogador = document.getElementById('jogador');
+    var opp3=document.getElementById("opp3");
+    var botao1 = document.getElementById("um");
+    var botao2 = document.getElementById("dois");
+    var botao3 = document.getElementById("tres");
     let jogo=true;
-    let quemJoga=gerarNumeroAleatorio(0, 2);
     do{
     let comida = gerarNumeroAleatorio(1,50);
     if(quemJoga%2==0){
+        h3Jogador.textContent = 'Wilson começa!';
         console.log("Wilson começa!");
         console.log("Voce pode:");
         console.log("1-jogar chuck na ilha, a cada jogada o o seu dano aumenta em +2");
         console.log("2-se defender, o dano da ilha sera cortado pela metade");  
         if(vezesAtacado>5 && comida%2==0){
+            opp3.style.display = 'block';
             console.log("Caiu uma comida surpresa no campo de batalha! Você tem mais uma opcção");
             console.log("3-comer, você pode comer e recuperar vida de forma levemente aleatoria");   
         }
-        let escolha=prompt("Qual sua escolha 1, 2 ou 3:");
+        //let escolha=prompt("Qual sua escolha 1, 2 ou 3:");
 
-        if(escolha==1){
+        /*if(escolha==1){
             ataqueHeroi();
         }
         else if(escolha==2){
@@ -44,12 +56,24 @@ function main(){
         }
         else{
             jogo=false;
-        }
+        }*/
+
+      
 
     }
     else{
+        h3Jogador.innerHTML = 'A ilha começa!';
         console.log("A ilha começa!");
         ataqueIlha();
+    }
+    if(adversario.vida<=0){
+        console.clear();
+        console.log("You Win");
+        jogo=false;
+    }
+    if(heroi.vida<=0){
+        console.log("Game Over");
+        jogo=false;
     }
     quemJoga++;
     }while(jogo)
@@ -57,17 +81,17 @@ function main(){
 function ataqueHeroi(){
     adversario.vida-=armaHeroi.dano;
     console.log("Wilson jogou chuck na ilha e tirou "+armaHeroi.dano+"de dano!");
-    console.log("A ilha ficou com "+adversario.vida+"de vida");
+    console.log("A ilha ficou com "+adversario.vida+" de vida");
     armaHeroi.dano+=2;
 }
 
 function defesaHeroi(){
-    armaAdversario.dano/=2
-    let dano=armaAdversario.dano;
-    heroi.vida-=dano;
+    armaAdversario.dano/=2;
+    heroi.vida-=armaAdversario.dano;
     console.log("Você se defendeu! no fim das contas...")
-    console.log("A ilha te deu "+armaAdversario.dano+"de dano!");
-    console.log("E Wilson ficou com "+heroi.vida+"de vida");
+    console.log("A ilha te deu "+armaAdversario.dano +"de dano!");
+    console.log("E Wilson ficou com "+heroi.vida+" de vida");
+    quemJoga++
 }
 
 function ataqueIlha(){
@@ -79,7 +103,7 @@ function ataqueIlha(){
         heroi.vida-=armaAdversario.dano;
     }
     console.log("A ilha te deu "+armaAdversario.dano+"de dano!");
-    console.log("E Wilson ficou com "+heroi.vida+"de vida");
+    console.log("E Wilson ficou com "+heroi.vida+" de vida");
     armaAdversario.dano+=1.5;
     vezesAtacado++;
 }
@@ -95,13 +119,14 @@ function recuperarVida(comida){
     }
     else if(comida>=2 && comida<10){
         heroi.vida+=30;
-        console.log("Wilson comeu um bife enorme, e recuperou 65 de vida, agora esta com "+heroi.vida);
+        console.log("Wilson comeu um bife enorme, e recuperou 30 de vida, agora esta com "+heroi.vida);
     }
     else{
         heroi.vida+=65;
         armaHeroi.dano*=armaHeroi.dano;
         console.log("Wilson comeu um XIS RAFAO, e recuperou 65 de vida, agora esta com "+heroi.vida);
-        console.log("E agora seu dano foi dobrado");
+        console.log("E agora seu dano foi dobrado e o adversario não joga");
+        quemJoga++;
     }
 
     return heroi.vida;
